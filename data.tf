db73db27b5f6c5063bef5e38951034b990737d3b
Shoaib <shoaibs411@gmail.com>
2024-01-18 02:23:52 +0530

    Creating Mutable App - TF AutoPush - Configuring mutable-app

---
+++ data.tf
@@ -0,0 +1,17 @@
+# Ensure the AMI that you use has ANSIBLE Installed.
+
+data "aws_ami" "image" {
+  most_recent      = true
+  name_regex       = "centos7-with-ansible"
+  owners           = ["355449129696"]              # Use your account number
+}
+
+# Datasource to fetch the information from the VPC Remote Statefile
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket  = "b56-terraform-state--bucket"
+    key     = "vpc/${var.ENV}/terraform.tfstate"
+    region  = "us-east-1"
+  }
+}
