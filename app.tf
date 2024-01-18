fef9d8b1b2aacad16425a57f7b9bfe57a8e5a0d8
Shoaib <shoaibs411@gmail.com>
2024-01-19 01:21:01 +0530

    Creating Mutable App - TF AutoPush - Configuring mutable-app

---
+++ app.tf
@@ -0,0 +1,18 @@
+resource "null_resource" "app_deploy" {
+    count      = local.INSTANCE_COUNT
+  provisioner "remote-exec" {
+
+    # connection block establishes connection to this
+    connection {
+      type     = "ssh"
+      user     = local.SSH_USERNAME
+      password = local.SSH_PASSWORD
+      host     = element(local.INSTANCE_IPS, count.index)            # aws_instance.sample.private_ip : Use this only if your provisioner is outside the resource.
+    }
+
+    inline = [
+        "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/ansible/install.sh | sudo bash",
+        "ansible-pull -U https://github.com/Shoaibs411/ansible.git -e APP_VERSION=${var.APP_VERSION} -e ENV=${var.ENV} -e COMPONENT=${var.COMPONENT}  roboshop-pull.yml"
+        ]
+    }
+}
--- data.tf
+++ data.tf
@@ -25,3 +25,13 @@ data "terraform_remote_state" "alb" {
     region  = "us-east-1"
   }
 }
+
+# Extracting the information of the secrects
+data "aws_secretsmanager_secret" "secrets" {
+  name = "roboshop/secrets"
+}
+
+# Extracting the secrect version(value) from the secrets
+data "aws_secretsmanager_secret_version" "secret_version" {
+  secret_id = data.aws_secretsmanager_secret.secrets.id
+}
